cca741f50bc21fb6d5a216674350801343134577
方长青 <752105005@qq.com>
2018-03-15 22:01:21 +0800

    三级菜单

---
+++ test1/day1/3level_menus.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+# author:Administrator
+# datetime:2018/3/15 19:53
+# software: PyCharm
+
+menu = {
+    '北京':{
+        '海淀':{
+            '五道口':{
+                'soho':{},
+                '网易':{},
+                'google':{}
+            },
+            '中关村':{
+                '爱奇艺':{},
+                '汽车之家':{},
+                'youku':{},
+            },
+            '上地':{
+                '百度':{},
+            },
+        },
+        '昌平':{
+            '沙河':{
+                '老男孩':{},
+                '北航':{},
+            },
+            '天通苑':{},
+            '回龙观':{},
+        },
+        '朝阳':{},
+        '东城':{},
+    },
+    '上海':{
+        '闵行':{
+            "人民广场":{
+                '炸鸡店':{}
+            }
+        },
+        '闸北':{
+            '火车战':{
+                '携程':{}
+            }
+        },
+        '浦东':{},
+    },
+    '山东':{},
+}
+
+
+exit_flag = False
+current_layer = menu
+
+layers = [menu]
+
+while not  exit_flag:
+    for k in current_layer:
+        print(k)
+    choice = input(">>:").strip()
+    if choice == "b":
+        current_layer = layers[-1]
+        #print("change to laster", current_layer)
+        layers.pop()
+    elif choice not  in current_layer:continue
+    else:
+        layers.append(current_layer)
+        current_layer = current_layer[choice]
